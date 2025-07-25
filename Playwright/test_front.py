5fbdc68b2380f00bcb05d5e827dff8e5eeb103cc
Лыжин Алексей Вячеславович <aleksey.lyzhin@rt.ru>
2025-07-25 10:30:12 +0500

    gitignore

---
+++ Playwright/test_front.py
@@ -0,0 +1,13 @@
+from playwright.sync_api import sync_playwright
+
+BASE_URL = 'https://www.saucedemo.com/'
+
+def start_front_test():
+    playwright = sync_playwright().start()
+
+    browser = playwright.chromium.launch(headless=False, slow_mo=50)
+    page = browser.new_page()
+    page.goto(BASE_URL)
+
+    browser.close()
+    playwright.stop()
