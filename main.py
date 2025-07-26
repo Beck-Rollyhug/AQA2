9587d10dde257a0feb1165d4a3d0469184897b44
Багоюзер <my.beck.for.work@mail.ru>
2025-07-26 14:36:12 +0500

    add to main

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 
+from Practice1.basic_info import task1
+from Practice1.user_profile import task2
+from Practice1.test_case_stats import task3
+from Practice1.bug_reports import task4
+from Practice1.bug_details import task5
+
+from Practice2.functions_intro import task6
+from Practice2.age_message import task7
+from Practice2.number_sum import task8
+from Practice2.triangle_check import task9
+from Practice2.nested_functions import task10
+
 from OOP.oop import CheckingAccount
 from Booker.test_booker import TestBookings
 from Playwright.test_front import start_front_test
 
+# Базовые задания на Python
+# task1()
+# task2()
+# task3()
+# task4()
+# task5()
+# task6()
+# task7()
+# task8()
+# task9()
+# task10()
+#
 
 # Задание с ООП
-savingsAccount = CheckingAccount('Alex', 500)
-savingsAccount.withdraw(100)
-savingsAccount.test_balance()
-savingsAccount.apply_interest()
+# savingsAccount = CheckingAccount('Alex', 500)
+# savingsAccount.withdraw(100)
+# savingsAccount.test_balance()
+# savingsAccount.apply_interest()
 #
 
 # Задание с restful-booker
-booker = TestBookings()
-
+# booker = TestBookings()
 # booker.test_create_booking()
 # booker.test_update_booking()
 # booker.test_partial_update_booking()
@@ -27,5 +50,5 @@ booker = TestBookings()
 #
 
 # Задание с Playwright
-start_front_test()
+# start_front_test()
 #
